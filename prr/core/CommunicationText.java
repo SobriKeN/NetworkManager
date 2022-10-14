c1fb99f7a58c4f0a52393cbde9883e98de02db0b
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-14 22:42:04 +0100

    created CommunicationInteractive.java

--- prr/core/CommunicationText.java
+++ prr/core/CommunicationText.java
@@ -7,33 +7,20 @@ public class CommunicationText extends Communication implements Serializable {
     /** Serial number for serialization. */
     private static final long serialVersionUID = 2062487349214690L;
 
-    /** Communication's id, the first comm starts with 1 */
-    private int id = 0;
-
-    /** To check if the communication has been paid */
-    private boolean isPaid;
-
-    /** Communication's cost*/
-    double cost;
-
-    /** To check if the communication is still ocurring */
-    boolean isOngoing;
-
     /** The text message */
     private String message;
 
+    /** The type of comm (in this case is text)  */
+    private String type;
+
     /**
      * Main Construtor
-     * @param id
-     * @param isPaid
-     * @param cost
-     * @param isOngoing
      * @param message
      */
-    public CommunicationText(int id, boolean isPaid, double cost, boolean isOngoing, String message) {
-        super(isPaid,cost,isOngoing);
-        this.id++;
+    public CommunicationText(String message) {
+        super();
         this.message = message;
+        this.type = "TEXT";
     }
 
     /** @return the cost of the text message */
