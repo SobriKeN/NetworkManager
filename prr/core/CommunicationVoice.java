bf148364b0e98eab9da5fd40ce0cd3601280a469
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-14 22:42:43 +0100

    created CommunicationVoice.java

--- prr/core/CommunicationVoice.java
+++ prr/core/CommunicationVoice.java
@@ -1,4 +1,26 @@
 package prr.core;
 
-public class CommunicationVoice {
+import java.io.Serializable;
+
+public class CommunicationVoice extends CommunicationInteractive implements Serializable {
+
+    /** Serial number for serialization. */
+    private static final long serialVersionUID = 5756840065019584737L;
+
+    /** The type of comm (in this case is video)  */
+    private String type;
+    /**
+     * Main Construtor
+     *
+     * @param duration
+     */
+    public CommunicationVoice(int duration) {
+        super(duration);
+        this.type = "VOICE";
+    }
+
+    @Override
+    protected double computeCost(TariffPlan plan) {
+        return 0;
+    }
 }
