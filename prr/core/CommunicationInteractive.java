c4fb344d58c0abd046abd68e37aeadb80843c9c0
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-14 22:42:50 +0100

    created CommunicationVoice.java

--- prr/core/CommunicationInteractive.java
+++ prr/core/CommunicationInteractive.java
@@ -1,4 +1,26 @@
 package prr.core;
 
-public abstract class CommunicationInteractive {
+import java.io.Serializable;
+
+public abstract class CommunicationInteractive extends Communication implements Serializable {
+
+    /** Serial number for serialization. */
+    private static final long serialVersionUID = 20782564287297L;
+
+    /** The duration of the call */
+    private final int duration;
+
+    /**
+     * Main Construtor
+     * @param duration
+     */
+    public CommunicationInteractive(int duration){
+        super();
+        this.duration = duration;
+    }
+
+    @Override
+    protected int getSize() {
+        return duration;
+    }
 }
