83f22c014639b87c45b20f4d826b40e2491857d1
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-16 18:15:24 +0100

    TariffPLan e so a interface para fazer os planos tarifarios, como so temos um vale apenas ter os metodos abstratos

--- prr/core/TariffPlan.java
+++ prr/core/TariffPlan.java
@@ -13,58 +13,10 @@ public abstract class TariffPlan {
         return this._name;
     }
 
-    protected double computeCost(Client c, CommunicationText t){
-        int custo = 0;
+    protected abstract double computeCost(Client c, CommunicationText t);
 
-        if (Objects.equals(c.get_level().toString(), "NORMAL")) {
-            if (t.getSize() < 50)
-                custo = 10;
-            else if (50 <= t.getSize() && t.getSize() < 100)
-                custo = 16;
-            else
-                custo = 2 * t.getSize();
-        }
-        if (Objects.equals(c.get_level().toString(), "GOLD")) {
-            if (t.getSize() < 50)
-                custo = 10;
-            else if (50 <= t.getSize() && t.getSize() < 100)
-                custo = 10;
-            else
-                custo = 2 * t.getSize();
-        }
-        if (Objects.equals(c.get_level().toString(), "PLATINUM")) {
-            if (t.getSize() < 50)
-                custo = 0;
-            else if (50 <= t.getSize() && t.getSize() < 100)
-                custo = 4;
-            else
-                custo = 4;
-        }
-        return custo;
-    }
+    protected abstract double computeCost(Client c, CommunicationVoice t);
 
-    protected double computeCost(Client c, CommunicationVoice t){
-        int custo = 0;
-
-        if (Objects.equals(c.get_level().toString(), "NORMAL"))
-            custo = 20 * (t.getDuration() / 60);
-        else if (Objects.equals(c.get_level().toString(), "GOLD"))
-            custo = 10 * (t.getDuration() / 60);
-        else if (Objects.equals(c.get_level().toString(), "PLATINUM"))
-            custo = 10 * (t.getDuration() / 60);
-        return custo;
-    }
-
-    protected double computeCost(Client c, CommunicationVideo t){
-        int custo = 0;
-
-        if (Objects.equals(c.get_level().toString(), "NORMAL"))
-            custo = 30 * (t.getDuration() / 60);
-        else if (Objects.equals(c.get_level().toString(), "GOLD"))
-            custo = 20 * (t.getDuration() / 60);
-        else if (Objects.equals(c.get_level().toString(), "PLATINUM"))
-            custo = 10 * (t.getDuration() / 60);
-        return custo;
-    }
+    protected abstract double computeCost(Client c, CommunicationVideo t);
 
 }
