da7846f7c41671c529b61c2e1929b468329de221
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-29 21:02:45 +0100

    mudar a cena do terminal

--- prr/app/terminal/DoTurnOffTerminal.java
+++ prr/app/terminal/DoTurnOffTerminal.java
@@ -2,6 +2,7 @@ package prr.app.terminal;
 
 import prr.core.Network;
 import prr.core.Terminal;
+import prr.core.TerminalMode;
 import pt.tecnico.uilib.menus.CommandException;
 //FIXME add more imports if needed
 
@@ -16,6 +17,10 @@ class DoTurnOffTerminal extends TerminalCommand {
   
   @Override
   protected final void execute() throws CommandException {
-    //FIXME implement command
+    if(_receiver.getTerminalModeEnum() == TerminalMode.OFF){
+      _display.popup(Message.alreadyOff());
+    }
+    _receiver.turnOff();
   }
 }
+
