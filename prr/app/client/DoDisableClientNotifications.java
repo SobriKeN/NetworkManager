ff884b70581ef25285da7431a4bbb66d740ac2da
thomastabs <tomassantostaborda@gmail.com>
2022-10-29 18:11:55 +0100

    DoEnableClientNotifications e DoDisableClientNotifications

--- prr/app/client/DoDisableClientNotifications.java
+++ prr/app/client/DoDisableClientNotifications.java
@@ -2,6 +2,7 @@ package prr.app.client;
 
 import prr.core.Network;
 import prr.app.exception.UnknownClientKeyException;
+import prr.core.exception.InvalidClientIDException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 //FIXME add more imports if needed
@@ -13,11 +14,18 @@ class DoDisableClientNotifications extends Command<Network> {
 
   DoDisableClientNotifications(Network receiver) {
     super(Label.DISABLE_CLIENT_NOTIFICATIONS, receiver);
-    //FIXME add command fields
+    addStringField("clientKey", Message.key());
   }
-  
+
   @Override
   protected final void execute() throws CommandException {
-    //FIXME implement command
+    try {
+      if (_receiver.getClient(stringField("clientKey")).isRecieveNotifications())
+        _receiver.getClient(stringField("clientKey")).deactivateNotifications();
+      else
+        _display.popup(Message.clientNotificationsAlreadyDisabled());
+    } catch (InvalidClientIDException e) {
+      throw new UnknownClientKeyException(e.getID());
+    }
   }
 }
--- prr/app/client/DoEnableClientNotifications.java
+++ prr/app/client/DoEnableClientNotifications.java
@@ -2,6 +2,7 @@ package prr.app.client;
 
 import prr.core.Network;
 import prr.app.exception.UnknownClientKeyException;
+import prr.core.exception.InvalidClientIDException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 //FIXME add more imports if needed
@@ -13,11 +14,18 @@ class DoEnableClientNotifications extends Command<Network> {
 
   DoEnableClientNotifications(Network receiver) {
     super(Label.ENABLE_CLIENT_NOTIFICATIONS, receiver);
-    //FIXME add command fields
+    addStringField("clientKey", Message.key());
   }
-  
+
   @Override
   protected final void execute() throws CommandException {
-    //FIXME implement command
+    try {
+      if (!_receiver.getClient(stringField("clientKey")).isRecieveNotifications())
+        _receiver.getClient(stringField("clientKey")).activateNotifications();
+      else
+        _display.popup(Message.clientNotificationsAlreadyEnabled());
+    } catch (InvalidClientIDException e) {
+      throw new UnknownClientKeyException(e.getID());
+    }
   }
 }
