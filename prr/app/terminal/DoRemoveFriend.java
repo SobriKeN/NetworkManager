19970231d2af69d2219155fbc76677841c7ad664
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-29 20:38:45 +0100

    lol

--- prr/app/terminal/DoRemoveFriend.java
+++ prr/app/terminal/DoRemoveFriend.java
@@ -4,7 +4,7 @@ import prr.core.Network;
 import prr.core.Terminal;
 import prr.core.exception.InvalidTerminalIDException;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
+
 
 /**
  * Remove friend.
