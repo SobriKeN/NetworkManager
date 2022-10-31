0a3c6a661cb43ef2e4cd1f9a57ed71118838a62c
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2022-10-31 14:24:59 +0000

    serializable

--- prr/core/DebtsAscendingOrder.java
+++ prr/core/DebtsAscendingOrder.java
@@ -1,8 +1,13 @@
 package prr.core;
 
+import java.io.Serial;
 import java.util.Comparator;
+import java.io.Serializable;
+public class DebtsAscendingOrder implements Comparator<Client>, Serializable {
 
-public class DebtsAscendingOrder implements Comparator<Client> {
+    /** Serial number for serialization **/
+    private static final long serialVersionUID = -5666240177742759000L;
+
     public int compare(Client c1, Client c2){
         return Long.compare(c1.getDebts(), c2.getDebts());
     }
--- prr/core/DebtsDescendingOrder.java
+++ prr/core/DebtsDescendingOrder.java
@@ -1,8 +1,14 @@
 package prr.core;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class DebtsDescendingOrder implements Comparator<Client> {
+public class DebtsDescendingOrder implements Comparator<Client>, Serializable {
+
+    /** Serial number for serialization **/
+    private static final long serialVersionUID = -6220091747558175800L;
+
     @Override
     public int compare(Client c1, Client c2){
         return Long.compare(c2.getDebts(), c1.getDebts());
