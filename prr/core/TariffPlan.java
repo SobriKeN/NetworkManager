3a6bdbfd90f47c746aa656de3178452d49add53d
thomastabs <tomassantostaborda@gmail.com>
2022-10-31 14:16:22 +0000

    plano de tarifario - just 1 now

--- prr/core/TariffPlan.java
+++ prr/core/TariffPlan.java
@@ -4,10 +4,10 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-public abstract class TariffPlan implements Serializable {
+public class TariffPlan implements Serializable {
 
     /** Serial number */
-    private static final long serialVersionUID = 2140256270744373855L;
+    private static final long serialVersionUID = 5230596851456277672L;
 
     /** specific name of the TariffPlan **/
     private final String _name;
@@ -31,10 +31,77 @@ public abstract class TariffPlan implements Serializable {
      * they will have similar ways of working but different arguments,
      * depending on the Communication type
      **/
-    protected abstract double computeCost(Client c, CommunicationText t);
 
-    protected abstract double computeCost(Client c, CommunicationVoice t);
+    /**
+     * @return the Cost of the TariffPlan decided by some conditions and rules set by the
+     * Client's status and balance; this method is only for Text Comms
+     * @param Client c
+     * @param CommunicationText t
+     */
+    public double computeCost(Client c, CommunicationText t){
+        int custo = 0;
 
-    protected abstract double computeCost(Client c, CommunicationVideo t);
+        if (Objects.equals(c.getLevel().toString(), "NORMAL")) {
+            if (t.getSize() < 50)
+                custo = 10;
+            else if (50 <= t.getSize() && t.getSize() < 100)
+                custo = 16;
+            else
+                custo = 2 * t.getSize();
+        }
+        if (Objects.equals(c.getLevel().toString(), "GOLD")) {
+            if (t.getSize() < 50)
+                custo = 10;
+            else if (50 <= t.getSize() && t.getSize() < 100)
+                custo = 10;
+            else
+                custo = 2 * t.getSize();
+        }
+        if (Objects.equals(c.getLevel().toString(), "PLATINUM")) {
+            if (t.getSize() < 50)
+                custo = 0;
+            else if (50 <= t.getSize() && t.getSize() < 100)
+                custo = 4;
+            else
+                custo = 4;
+        }
+        return custo;
+    }
+
+    /**
+     * @return the Cost of the TariffPlan decided by some conditions and rules set by the
+     * Client's status and balance; this method is only for Voice Comms
+     * @param Client c
+     * @param CommunicationVoice t
+     */
+    public double computeCost(Client c, CommunicationVoice t){
+        int custo = 0;
+
+        if (Objects.equals(c.getLevel().toString(), "NORMAL"))
+            custo = 20 * (t.getSize() / 60);
+        else if (Objects.equals(c.getLevel().toString(), "GOLD"))
+            custo = 10 * (t.getSize()/ 60);
+        else if (Objects.equals(c.getLevel().toString(), "PLATINUM"))
+            custo = 10 * (t.getSize() / 60);
+        return custo;
+    }
+
+    /**
+     * @return the Cost of the TariffPlan decided by some conditions and rules set by the
+     * Client's status and balance; this method is only for Video Comms
+     * @param Client c
+     * @param CommunicationVideo t
+     */
+    public double computeCost(Client c, CommunicationVideo t){
+        int custo = 0;
+
+        if (Objects.equals(c.getLevel().toString(), "NORMAL"))
+            custo = 30 * (t.getSize() / 60);
+        else if (Objects.equals(c.getLevel().toString(), "GOLD"))
+            custo = 20 * (t.getSize() / 60);
+        else if (Objects.equals(c.getLevel().toString(), "PLATINUM"))
+            custo = 10 * (t.getSize() / 60);
+        return custo;
+    }
 
 }
